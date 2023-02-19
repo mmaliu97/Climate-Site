615b6a7cf74ab3e9ea137ab19f1096da55305340
mmaliu97 <mmaliu@ucdavis.edu>
2023-02-19 15:08:34 -0800

    added simple scripts and folders for structure

---
+++ test.py
@@ -0,0 +1,29 @@
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+from python_scripts import test_script
+from python_scripts import others
+
+
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+script_testing = st.container()
+
+modelTraining = st.container()
+
+with header:
+  st.title('Climate-Site-napse')
+  st.text(others.add(10,22))
+  st.text('Insert description here')
+
+with script_testing:
+  st.header('Here I will test scripts')
+  number = test_script.add(10,20)
+  st.text(f'Insert description here {number}')
+
