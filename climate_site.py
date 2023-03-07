3fe1f518e5f7fefff378e6e8836bc5cb24609a3d
mmaliu97 <mmaliu@ucdavis.edu>
2023-03-07 11:20:06 -0800

    added radio buttons

--- climate_site.py
+++ climate_site.py
@@ -40,32 +40,40 @@ with patent_finder:
   st.write(f'You chose {tech_category}')
 
   # After a selection is made the choices will update
-  # category_widget = dic_categories[option]
-  # st.write(f'That belongs to the category {category_widget}')
-
   # Choose the technology and number
   technologies = dic_technologies[tech_category]
 
   # display only the tech name
-  key = lambda t: t[0]
-  technology = st.selectbox('Select a category',technologies,format_func = key)
-  st.write(f'You chose {technology[0]}')
-  
+  key0 = lambda t: t[0]
+  technology = st.selectbox('Select a category',technologies,format_func = key0)
+  # st.write(f'You chose {technology[0]}')
+  # st.write(f'You chose the tech number {technology[1]}')
+
+  # display the categories
+  tech_categories = dic_categories[technology[1]]
+  tech_category_keyword = st.radio(
+    "Select a keyword to search on: ",
+    tech_categories)
+
   # Select type of patent
-  patent_type = st.selectbox('Select a category',['Any related patents', 'Climate related patents'])
-  st.write(f'You chose {patent_type}')
+  list_climate = [ ("Any related patents" , False ) , ("Climate related patents" , True)]
+  patent_type_bool = st.selectbox('Select a category',list_climate,format_func = key0)
+  st.write(f'You chose {patent_type_bool[1]}')
 
-
-  if st.button('Get related patents'):
-    patent_rank_df = multi_input_functions.get_ranking_patents(
-    technologies = technology[0],
-    number_technology = technology[1],
-    carbon_related = patent_type,
-    category = tech_category,
-    size = 10
-  )
-    st.dataframe(patent_rank_df)
-
+  
+  try:
+    if st.button('Get related patents'):
+      patent_rank_df = multi_input_functions.get_ranking_patents(
+      technologies = technology[0],
+      number_technology = technology[1],
+      carbon_related = patent_type_bool[1],
+      category = tech_category_keyword,
+      size = 10
+    )
+      st.dataframe(patent_rank_df)
+      print(patent_rank_df)
+  except NameError:
+    st.write(f'No patents found')
   # st.write(f'list_categories_tech is {list_categories_tech}')
   # st.write(f'list_technologies is {list_technologies}')
   # st.write(f'dic_categories is {dic_categories}')
